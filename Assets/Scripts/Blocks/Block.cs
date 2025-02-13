34949492b704d518e7bb341b92e5582034f54394
Carlos Martínez <carlosssmrtzz@gmail.com>
2025-02-13 18:08:06 +0100

    Moneda

--- Assets/Scripts/Blocks/Block.cs
+++ Assets/Scripts/Blocks/Block.cs
@@ -6,13 +6,19 @@ public class Block : BlockBase
 {
     [SerializeField]
     private int coins;
+    public GameObject coin;
+    public GameObject bloqueInvencible;
 
     public void Hit() { 
         base.Hit();
+        Instantiate(coin,new Vector3(this.transform.position.x, this.transform.position.y + 1), Quaternion.identity);
 
         this.coins--;
-        if (coins == 0) { 
-            Destroy(this);
+        if (coins <= 0) {
+           
+            Instantiate(bloqueInvencible, this.transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+            
         }
     }
 }
